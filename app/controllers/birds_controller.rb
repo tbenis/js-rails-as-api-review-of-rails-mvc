4cc69eae33a7cb76260690ddd44d96bc100b7c7c
Benis Tambe <will.jenifer@yahoo.com>
2021-08-10 12:40:23 -0400

    commit tree ada6c9852e5f067655c1f0ba79eeb7f4f2374373

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,15 @@
 # Add code from Readme
+
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+end
+
+#   class BirdsController < ApplicationController
+#     def index
+#       @birds = Bird.all
+#       render plain: "Hello #{@birds[3].name}"
+#     end
+#   end
